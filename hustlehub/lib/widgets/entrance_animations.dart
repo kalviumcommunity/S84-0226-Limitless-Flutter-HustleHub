a4721569cae4d3419d2df11545e415310500e9f9
rohitmehta18 <rmehta1836@gmail.com>
2026-03-23 09:20:03 +0530

    transitition

---
+++ hustlehub/lib/widgets/entrance_animations.dart
@@ -0,0 +1,224 @@
+import 'package:flutter/material.dart';
+
+class FadeInAnimation extends StatefulWidget {
+  final Widget child;
+  final Duration duration;
+  final Duration delay;
+  final Curve curve;
+
+  const FadeInAnimation({
+    super.key,
+    required this.child,
+    this.duration = const Duration(milliseconds: 800),
+    this.delay = Duration.zero,
+    this.curve = Curves.easeInOut,
+  });
+
+  @override
+  State<FadeInAnimation> createState() => _FadeInAnimationState();
+}
+
+class _FadeInAnimationState extends State<FadeInAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.duration,
+    );
+
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: widget.curve),
+    );
+
+    Future.delayed(widget.delay, () {
+      if (mounted) {
+        _controller.forward();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(opacity: _fadeAnimation, child: widget.child);
+  }
+}
+
+class SlideInAnimation extends StatefulWidget {
+  final Widget child;
+  final Duration duration;
+  final Duration delay;
+  final Curve curve;
+  final Offset begin;
+
+  const SlideInAnimation({
+    super.key,
+    required this.child,
+    this.duration = const Duration(milliseconds: 800),
+    this.delay = Duration.zero,
+    this.curve = Curves.easeInOut,
+    this.begin = const Offset(-1.0, 0.0),
+  });
+
+  @override
+  State<SlideInAnimation> createState() => _SlideInAnimationState();
+}
+
+class _SlideInAnimationState extends State<SlideInAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.duration,
+    );
+
+    _slideAnimation = Tween<Offset>(begin: widget.begin, end: Offset.zero)
+        .animate(
+          CurvedAnimation(parent: _controller, curve: widget.curve),
+        );
+
+    Future.delayed(widget.delay, () {
+      if (mounted) {
+        _controller.forward();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(position: _slideAnimation, child: widget.child);
+  }
+}
+
+class ScaleInAnimation extends StatefulWidget {
+  final Widget child;
+  final Duration duration;
+  final Duration delay;
+  final Curve curve;
+
+  const ScaleInAnimation({
+    super.key,
+    required this.child,
+    this.duration = const Duration(milliseconds: 800),
+    this.delay = Duration.zero,
+    this.curve = Curves.elasticOut,
+  });
+
+  @override
+  State<ScaleInAnimation> createState() => _ScaleInAnimationState();
+}
+
+class _ScaleInAnimationState extends State<ScaleInAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.duration,
+    );
+
+    _scaleAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: widget.curve),
+    );
+
+    Future.delayed(widget.delay, () {
+      if (mounted) {
+        _controller.forward();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleTransition(scale: _scaleAnimation, child: widget.child);
+  }
+}
+
+class BounceInAnimation extends StatefulWidget {
+  final Widget child;
+  final Duration duration;
+  final Duration delay;
+
+  const BounceInAnimation({
+    super.key,
+    required this.child,
+    this.duration = const Duration(milliseconds: 900),
+    this.delay = Duration.zero,
+  });
+
+  @override
+  State<BounceInAnimation> createState() => _BounceInAnimationState();
+}
+
+class _BounceInAnimationState extends State<BounceInAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _bounceAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.duration,
+    );
+
+    _bounceAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.elasticOut),
+    );
+
+    Future.delayed(widget.delay, () {
+      if (mounted) {
+        _controller.forward();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: _bounceAnimation.value,
+      child: Transform.scale(
+        scale: _bounceAnimation.value,
+        child: widget.child,
+      ),
+    );
+  }
+}
